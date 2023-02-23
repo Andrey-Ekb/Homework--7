8009b5a567dc84006bdd4abaa34c70c9bdff6b71
Andrey <Andre.68.1@mail.ru>
2023-02-23 13:02:38 +0500

    string slicing

---
+++ Homework7_002/Program.cs
@@ -0,0 +1,65 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+Console.Clear();
+
+int ReadInt()
+{
+    Console.Write("Введите размер массива : ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+void FillArray (int[,] array)
+{
+    Random rnd=new Random();
+        for (int i=0; i<array.GetLength(0); i++)
+        
+            for (int j=0; j < array.GetLength(1); j++)
+            { 
+                array[i,j] = rnd.Next(1,10);
+            }
+}
+void PrintArray (int[,]array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    { 
+        for (int j=0; j < array.GetLength(1); j++)
+        {     
+            Console.Write($"{ array[i,j] }\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortRowAscending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+            }
+            int tmp = array[i, j];                                          
+            array[i, j] = array[i, minPosition];                            
+            array[i, minPosition] = tmp;                                    
+        }
+
+    }
+    return array;
+}
+
+Console.WriteLine();
+int m=ReadInt();
+int n=ReadInt();
+Console.WriteLine();
+
+int[,]array=new int[m,n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int[,] rowSort = SortRowAscending(array);
+PrintArray(array);
+Console.WriteLine();
