73edeffda769b2a590f87952f9511694e9a20769
Andrey <Andre.68.1@mail.ru>
2023-02-23 13:09:33 +0500

    string with minimum sum of elements

---
+++ Homework7_003/Program.cs
@@ -0,0 +1,81 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+Console.Clear();
+
+int ReadInt()
+{
+    Console.Write("Введите размер массива : ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+void FillArray (int[,] array)
+{
+    Random rnd=new Random();
+        for (int i=0; i<array.GetLength(0); i++)
+        
+            for (int j=0; j < array.GetLength(1); j++)
+            { 
+                array[i,j] = rnd.Next(1,10);
+            }
+}
+void PrintArray (int[,]array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    { 
+        for (int j=0; j < array.GetLength(1); j++)
+        {     
+            Console.Write($"{ array[i,j] }\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SmallestString (int[,]array)
+{
+    Console.WriteLine();
+    int n=array.GetLength(0);
+    int[]rez=new int[n]; 
+    int min=0;
+    int k=1;
+
+        for(int i=0; i<array.GetLength(0); i++)
+        {
+            for(int j=0; j<array.GetLength(1); j++)
+            { 
+                rez[i]=rez[i] + array[i,j];
+                k=i;
+            }
+            Console.Write(rez[i]+"-"+(k+1)+"  ");   
+        }
+
+        Console.WriteLine();
+
+        min=rez[0];
+
+        for(int i=0; i<rez.Length; i++)
+        {
+            if(min>rez[i])
+                min=rez[i];
+        }
+
+    Console.WriteLine();
+
+        for(int i=0; i<rez.Length; i++)
+            if(min==rez[i])
+                Console.WriteLine(" строка с наименьшей суммой элементов найденa, значение элементов - "+ min +" строка №  "+ (i+1));
+
+}
+
+Console.WriteLine();
+int m=ReadInt();
+int n=ReadInt();
+Console.WriteLine();
+
+int[,]array=new int[m,n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SmallestString(array);
+Console.WriteLine();
+
